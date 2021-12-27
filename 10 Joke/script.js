7348f0443148fec0f1f7c977b764c4fe6bd062ce
Minh Hoang <hoangvanminh007@gmail.com>
2021-12-27 20:51:23 +0700

    new pratice

---
+++ 10 Joke/script.js
@@ -0,0 +1,32 @@
+const jokeEl = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+generateJoke();
+//  NOTE: async
+jokeBtn.addEventListener("click", generateJoke);
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
