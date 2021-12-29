0f74c3bc982d54b9d6d3c9fda24ca3d463ebdf4d
Minh Hoang <hoangvanminh007@gmail.com>
2021-12-29 14:08:04 +0700

    keycode

---
+++ 10 - keycode/script.js
@@ -0,0 +1,20 @@
+const insert = document.querySelector(".insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+    <div class="key">
+        ${e.key === " " ? "Space" : e.key }
+        <small>Event.key</small>
+      </div>
+      <!--  -->
+      <div class="key">
+        ${e.keyCode}
+        <small>Event.keyCode</small>
+      </div>
+      <!--  -->
+      <div class="key">
+        ${e.code}
+        <small>Event.code</small>
+      </div>
+      <!--  -->`;
+});
